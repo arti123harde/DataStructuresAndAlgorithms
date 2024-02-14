6a92cc42bd7f29943bba48abaa90b1860001f0be
Arti Harde <arti.harde@walmart.com>
2024-02-15 02:27:10 +0530

    initial commit

--- src/main/java/org/solutions/string/CalculateDigitSumOfString.java
+++ src/main/java/org/solutions/string/CalculateDigitSumOfString.java
@@ -8,4 +8,9 @@ public class CalculateDigitSumOfString {
     public static void main(String[] args) {
 
     }
+
+    public static String digitSum(String s, int k) {
+        String s1 = "";
+        return s1;
+    }
 }
